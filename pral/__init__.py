e0d181cf2385f1d10b7372b15ec0cb9fd250ecbf
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-09-28 08:45:58 -0500

    fix size

--- pral/__init__.py
+++ pral/__init__.py
@@ -36,7 +36,7 @@ class _InputParamRange:
     external_field_A: tuple[float, float] = (-10.0, 10)
     external_field_B: tuple[float, float] = (-10.0, 10.0)
     external_field_C: tuple[float, float] = (-10.0, 10.0)
-    growth_factor: tuple[float, float] = (0, 1.0)
+    growth_factor: tuple[float, float] = (-0.4, 1.0)
 
 
 @dataclass
@@ -106,16 +106,16 @@ class SystemParamter:
         DL = 1 / 7.0
         volume = 5.0
         if name == "small":
-            L = np.asarray([volume / (1 + 2 * self.growth_factor), 1 + 2 * self.growth_factor, 1.0])
+            L = np.asarray([volume / (1 + 2 * self.growth_factor), 1.0, 1 + 2 * self.growth_factor])
             NL = (np.asarray(L) / DL).astype(int)
         if name == "medium":
-            L = np.asarray([volume / (1 + self.growth_factor), 1 + self.growth_factor, 1.0])
+            L = np.asarray([volume / (1 + self.growth_factor), 1.0, 1 + self.growth_factor])
             NL = (np.asarray(L) / DL).astype(int)
         if name == "large":
-            L = np.asarray([volume, 1, 1.0])
+            L = np.asarray([volume, 1.0, 1.0])
             NL = (np.asarray(L) / DL).astype(int)
         # Add for area 51
-        NL[0] += 1
+        NL[2] += 1
         self._system_param = SystemParamter._InternalSystemParam(
             name=name,
             n_homo=n_homo,
